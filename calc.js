b49ea55d41e9cccf389307ea413e5368e6ced2a6
Caleb Hodges <chodgesdev@gmail.com>
2022-11-12 16:10:18 -0600

    added square root string to identify sqrt operations

--- calc.js
+++ calc.js
@@ -14,9 +14,6 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
-	console.log('mathSymbol', mathSymbol);
-	console.log('num1', num1);
-	console.log('num2', num2);
 
 	if (mathSymbol === "+"){
 		console.log(num1 + num2)
@@ -31,7 +28,7 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 / num2)
 	}
 	if (mathSymbol === "√"){
-		console.log(Math.sqrt(num1))
+		console.log('√ =', Math.sqrt(num1))
 	}
 
 	//Square root symbol is "Opt v"
